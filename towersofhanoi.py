545f7d36d1eca442f08e6a147fc80265715cd70e
David Ecker <de846@nyu.edu>
2016-11-08 21:01:55 -0500

    add discs to left column during init, assume heavy discs are bigger ints

--- towersofhanoi.py
+++ towersofhanoi.py
@@ -9,10 +9,15 @@ class HanoiTower(object):
        self._col_mid = []
        self._col_right = []
        self._num_discs = num_discs
+       for i in range(self._num_discs, 0, -1):
+           self._col_left.append(i)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Towers of Hanoi")
-    parser.add_argument('-n', '--num-discs', help='Number of discs until the universe ends')
+    parser.add_argument('-n', '--num-discs', help='Number of discs until the universe ends', default=DEFAULT_NUM_DISCS, type=int)
     args = parser.parse_args()
+
+    tower = HanoiTower(args.num_discs)
+    print(tower._col_left)
 
