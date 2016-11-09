c08295888b00c9e425462e8e6fea02b7fa19d154
David Ecker <de846@nyu.edu>
2016-11-08 20:46:03 -0500

    Input number of discs, or have a default (arbitrarily 8)

--- towersofhanoi.py
+++ towersofhanoi.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
+import argparse
+
+DEFAULT_NUM_DISCS = 8
 
 class HanoiTower(object):
-    def __init__(self):
+    def __init__(self, num_discs=DEFAULT_NUM_DISCS):
        self._col_left = []
        self._col_mid = []
        self._col_right = []
+       self._num_discs = num_discs
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Towers of Hanoi")
+    parser.add_argument('-n', '--num-discs', help='Number of discs until the universe ends')
+    args = parser.parse_args()
+
